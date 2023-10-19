e51b47fc1058a72964dcde69b89536620843aee9
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-10-19 22:43:50 +0600

    Base Url Updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://booking-backend-theta.vercel.app/api/v1" }),
   tagTypes: [""],
   endpoints: () => ({}),
 });
